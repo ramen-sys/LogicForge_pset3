a8337ddc2293f8e047a49f20c654e911d872494c
ramen_sys <srameen888@gmail.com>
2026-01-17 13:38:00 +0500

    removed errors from challenge 2

--- challenge2.py
+++ challenge2.py
@@ -1,21 +1,24 @@
 N,Q,Limit=map(int,input().split())
 weight=list(map(int,input().split()))
 priority=list(map(int,input().split()))
+people=zip(weight,priority)
 
-weight.sort()
+
+
+people.sort()
 boat_count=0
 
 heavy=len(weight)-1
 light=0
 
 while light<=heavy:
-    if weight[heavy]+weight[light]<=Limit and not (priority[light]==1 and priority[heavy]==1):
-        pair="yes"
+    if weight[heavy]+weight[light]<=Limit and not (priority[light][1]==1 and priority[heavy][1]==1):
+
         heavy-=1
         light+=1
     else:
         heavy-=1
-        pair="no"
+
     boat_count+=1
 
 
@@ -25,7 +28,7 @@ for _ in range(Q):
     query=input().split()
 
     if query[0]=="CANPAIR":
-        if (weight[query[1]] + weight[query[2]] <= Limit) and not (priority[query[2]] == 1 and priority[query[2]]==1):
+        if (weight[int(query[1])] + weight[int(query[2])] <= Limit) and not (priority[int(query[1]]) == 1 and priority[int(query[2])]==1):
             print("Yes")
         else:
             print("No")
