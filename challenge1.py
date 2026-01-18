5d172cd4e2b76b999deab5d5ad715bd54d0e04c6
ramen_sys <srameen888@gmail.com>
2026-01-19 00:16:27 +0500

    added output file

--- challenge1.py
+++ challenge1.py
@@ -5,35 +5,35 @@ nextHot=[0]*N
 stack=[]
 
 for i in range(N):
-    while stack and temp[i]>=temp[stack[-1]+K]:
+    while stack and temp[i]>=temp[stack[-1]]+K:
         idx=stack.pop()
         nextHot[idx]=i
 
     stack.append(i)
 
-    nextcold=[]
-    stack.clear()
-    for i in range(N):
-        while stack and temp[i]<=temp[stack[-1]]-K:
-            idx=stack.pop()
-            nextcold[idx]=i
-        stack.append(i)
+nextcold=[0]*N
+stack.clear()
+for i in range(N):
+    while stack and temp[i]<=temp[stack[-1]]-K:
+        idx=stack.pop()
+        nextcold[idx]=i
+    stack.append(i)
 
-    alert=[0]*N
+alert=[0]*N
 
-    for i in range(N):
-        if nextHot[i]!=0 and nextcold[i]!=0:
-            alert[i]=min(nextHot[i],nextcold[i])
-        else:
-            alert[i]=max(nextHot[i],nextcold[i])
+for i in range(N):
+    if nextHot[i]!=0 and nextcold[i]!=0:
+        alert[i]=min(nextHot[i],nextcold[i])
+    else:
+        alert[i]=max(nextHot[i],nextcold[i])
 
-    pref=[0]*N
-    pref[0]=1 if alert[0] !=0 else 0
+pref=[0]*N
+pref[0]=1 if alert[0] !=0 else 0
 
-    for i in range(1,N):
-        pref[i]=pref[i-1]
-        if alert[i]!=0:
-            pref[i]+=1
+for i in range(1,N):
+    pref[i]=pref[i-1]
+    if alert[i]!=0:
+        pref[i]+=1
 
 for _ in range(Q):
     query=input().split()
